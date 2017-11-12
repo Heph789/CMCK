92722e46ca040dfd8be34f63e0bde173813cde22
Angie Barrington-Jeter <angiebarrington-jeter@Angies-MacBook-Pro.local>
2017-11-12 11:23:33 -0500

    Adds the backend functionality

--- ProductivityApp/src/app/activity.ts
+++ ProductivityApp/src/app/activity.ts
@@ -1,7 +1,43 @@
+import { Observable } from 'rxjs/Rx';
 export class Activity {
     name:string;
-    time:number;
+    totalTime:number = 0;
+    fime:number[] = [0, 0, 0];
     percentage:number;
+    private interval;
+    start():void {
+        /*let foo = function () {
+            this.fime[2] += 1;
+            if(this.fime[2] > 60) {
+                this.fime[1] += 1;
+                this.fime[2] = 0;
+            }
+        };*/
+        this.interval = setInterval(()=> {
+            this.fime[2] += 1;
+            if(this.fime[2] > 60) {
+                this.fime[1] += 1;
+                this.fime[2] = 0;
+            }
+        }, 1000);
+    }
+    /*private do():void {
+        this.fime[2] += 1;
+        if(this.fime[2] > 60) {
+            this.fime[1] += 1;
+            this.fime[2] = 0;
+        }
+    }*/
+    stop():void {
+        clearInterval(this.interval);
+    }
+    reset():void {
+        this.totalTime = 0;
+        this.fime[0] = 0;
+        this.fime[1] = 0;
+        this.fime[2] = 0;
+        this.percentage = 0;
+    }
     constructor(name:string) {
         this.name = name;
     }
--- ProductivityApp/src/app/app.component.ts
+++ ProductivityApp/src/app/app.component.ts
@@ -1,24 +1,35 @@
 import { Component } from '@angular/core';
 import { Activity } from './activity';
-
+import { Observable } from 'rxjs/Rx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    activities:Activity[] = [new Activity('Video Games'), new Activity('Homework')];
+    activities:Activity[] = [new Activity('Video Games'), new Activity('Homework'), new Activity('Sleep')];
     //this.activities.push(new Activity("Video Games"));
     //this.activities.push(new Activity("Homework"));
+    name:string;
     calc():void {
-        var totalTime:number;
+        let totalTime:number = 0;
         for (let obj of this.activities) {
-            totalTime += obj.time;
-            console.log(totalTime);
+            obj.totalTime += (obj.fime[0]*3600) + (obj.fime[1]*60) + obj.fime[2];
+            totalTime += obj.totalTime;
+            //totalTime += obj.time;
         }
         console.log(totalTime);
         for (let obj of this.activities) {
-            obj.percentage = obj.time/totalTime;
+            obj.percentage = (obj.totalTime/totalTime)*100;
+            if((obj.percentage - Math.floor(obj.percentage)) >= .5) {
+                obj.percentage = Math.ceil(obj.percentage);
+            }
+            else {
+                obj.percentage = Math.floor(obj.percentage);
+            }
         }
+    }
+    add():void {
+        this.activities.push(new Activity(this.name));
     }
 }
